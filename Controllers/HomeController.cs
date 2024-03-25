b0266688d4b4c29b952fbe1ab9f35898f5965215
Valter Backström <dylandante@gmail.com>
2024-03-25 12:33:55 +0200

    Roottori index näkymä laitettu Home index näkymään

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using RoottoriV1._2.Models;
 
 namespace RoottoriV1._2.Controllers
 {
     public class HomeController : Controller
     {
+        private RoottoriDBEntities2 db = new RoottoriDBEntities2();
         public ActionResult Index()
         {
-            return View();
+            var roottorit = db.Roottorit.Include(r => r.Koneet)
+                                        .Include(r => r.Karjet)
+                                        .Include(r => r.Leuat)
+                                        .Include(r => r.Magneetit)
+                                        .Include(r => r.Paletit)
+                                        .Include(r => r.Piirustukset).ToList();
+
+            return View("~/Views/Roottorit/Index.cshtml", roottorit);
         }
+
 
         public ActionResult About()
         {

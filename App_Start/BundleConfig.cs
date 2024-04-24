c04f652baa89b033d566a8662f09800a07b009c8
Jani Pekkala <pekkala.jani@hotmail.fi>
2024-04-24 11:10:23 +0300

    SiivottuMazak 400 ja Mazak 500 näkymiä. Bootstrap teemanvaihto.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace RoottoriV1._2
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrapmat.css",
                       "~/Content/site.css"));
         }
     }

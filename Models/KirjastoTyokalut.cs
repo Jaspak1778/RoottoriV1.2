a43ce1c0a35bda080f2386c27b4e7df36e78b6ef
Toni Pekkala <toni.pekkala@hotmail.com>
2024-06-05 09:44:17 +0300

    Virheen tarkistukset lisätty luokkiin. Lisätty myös puuttuva loppu tägi <th class"container">  KirjastoTyokalut sivulle

--- Models/KirjastoTyokalut.cs
+++ Models/KirjastoTyokalut.cs
@@ -9,9 +9,10 @@
 
 namespace RoottoriV1._2.Models
 {
+    using System.ComponentModel.DataAnnotations;
     using System;
     using System.Collections.Generic;
-    
+
     public partial class KirjastoTyokalut
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,18 +22,22 @@ namespace RoottoriV1._2.Models
             this.MalliE25RTyokalut = new HashSet<MalliE25RTyokalut>();
             this.MalliE6RTyokalut = new HashSet<MalliE6RTyokalut>();
         }
-    
+
         public int TyokaluID { get; set; }
         public Nullable<int> TyokaluKategoriaID { get; set; }
         public int KoneID { get; set; }
         public int TyokaluNro { get; set; }
+        //Lisätty virheiden tarkistusta varten
+
+        [Required(ErrorMessage = "Työkalun nimi on pakollinen")]
         public string TyokalunNimi { get; set; }
+
         public int Pituus { get; set; }
         public int Halkaisija { get; set; }
         public string Pala { get; set; }
         public string ImageLink { get; set; }
         public string Lisatieto1 { get; set; }
-    
+
         public virtual Koneet Koneet { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<MalliE25RiTyokalut> MalliE25RiTyokalut { get; set; }
@@ -42,3 +47,4 @@ namespace RoottoriV1._2.Models
         public virtual ICollection<MalliE6RTyokalut> MalliE6RTyokalut { get; set; }
     }
 }
+
